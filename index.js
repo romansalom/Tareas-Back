f3fd1fc24779cd85f1b6f9628b8fc0d46602344a
romansalom <roman.salom12@gmail.com>
2023-04-03 10:36:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const getProducts = []
+
+class ProductManager{
+    static id = 1;
+    
+     constructor(title , descriptions , price , thumbnail , code , stock , id){
+        this.title = title,
+        this.descriptions= descriptions,
+         this.price = price,
+        this.thumbnail = thumbnail ,
+        this.code =code ,
+        this.stock = stock,
+        ProductManager.id;
+     }
+   
+    
+addProduct(){
+
+    const productos = {
+       
+        title : this.title,
+        descriptions: this.descriptions,
+        price : this.price ,
+        thumbnail : this.thumbnail,
+        code :  this.code,
+        stock : this.stock,
+        id : ProductManager.id,
+        
+        }
+       
+       
+
+
+
+        const verificarCodigo = getProducts.find(elemento => elemento.code === productos.code) 
+           
+       if (verificarCodigo)
+       {
+        throw new Error('hay codes iguales')  }
+        else{
+            ProductManager.id ++ 
+            getProducts.push(productos);
+            
+        }
+
+       
+    
+    }
+    getById(id){
+        const getProducts =this.getAll()
+        if(!this.checkLength(getProducts)){
+            return
+        }
+        let productos = getProducts.find(elemento => elemento.id == id)
+        return productos ? productos : null
+    }
+    
+}
+
+
+
+
+let producto1 = new ProductManager("regla" , "30 cm" , 50 , "www.regla.com" , 1100 , 23 );
+
+
+producto1.addProduct();
+let producto2 = new ProductManager("escuadra" , "triangular" , 12, "www.escuadra.com" , 100 , 12);
+producto2.addProduct();
+let producto3 = new ProductManager("lapiz" , "de color" , 11, "www.lapizdecolores.com" , 1200 , 6);
+producto3.addProduct();
+
+console.log(getProducts);
